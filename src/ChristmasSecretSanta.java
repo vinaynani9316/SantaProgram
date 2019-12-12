a7d00eef6dbd1b45f2eb160ec6f9f88c161272af
vinay kumar <vinaynani93l@gmail.com>
2019-12-13 00:44:43 +0530

    first commit

---
+++ src/ChristmasSecretSanta.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ChristmasSecretSanta 
+{
+	public String[] PopAssignments(String[] names)
+	{
+		if(names== null)                 // in case of null input return null
+			return null;
+		
+		if(names.length ==0)            // in case of array with length 0 the return an empty String array
+			 return new String[0];
+		
+		if(names.length==1)           // in case of single element in array return the same array
+			return names;
+		
+		int nLength= names.length;
+		
+		Boolean[] chosen = new Boolean[nLength];
+		Boolean[] santa = new Boolean[nLength];
+		
+		Arrays.fill(chosen, false);                // Initializing array with false
+		Arrays.fill(santa, false); 
+		
+		Random r = new Random();
+		
+		String[] result = new String[names.length];              // result of array
+		int position=0;
+		
+		while(true)
+		{
+			int random1 =r.nextInt(nLength);
+			int random2 =r.nextInt(nLength);
+			
+			String person1= names[random1];                    // getting random person
+			String person2= names[random2];
+			
+			if(person1 == person2)
+			{
+				 
+			}
+			
+			if ((!person1.equals(person2)) && (santa[random1] == false)         // if two persons names are not same	
+			          && (chosen[random2] == false)) {
+			 
+			if(!person1.equals(names[position]))
+			{
+				result[position] =person1;
+				position++;
+				
+				santa[random1] = true;
+				chosen[random2] =true;
+				 
+				if (isArrayCompleted(santa) && isArrayCompleted(chosen))
+					break;
+			}
+		}
+	}
+       return result;
+}
+  private boolean isArrayCompleted(Boolean[] arr)                      // checking the array execution function
+  {
+	  for(int i=0; i<arr.length; i++ )
+	  {
+		  if(arr[i]== false)
+		  {
+			  return false;
+		  }
+	  }
+	  return true;
+  }
+    public static void main(String[] a)
+    {
+    	Scanner particpate= new Scanner(System.in);
+    	System.out.println("Enter the names");
+    	System.out.println("Enter assigned");
+    	String l= particpate.next();
+    	String s= particpate.next();
+    	
+    	final String[] participants =
+    			new String[] {l};
+    	
+    	
+    	final String[] assignments= new String[] {s};
+    	new ChristmasSecretSanta().PopAssignments(participants);
+    			
+    	for (int i=0; i<assignments.length; i++)
+    	{
+    		System.out.println(assignments[i]);
+    		if(assignments[i].equals(participants[i]))
+    		{
+    			System.out.println("Error: " +assignments[i]);
+    		}
+    	}
+    	particpate.close();
+    }
+   
+ }
